90b97963dc994fc81e76b4bc5379997a2dd56cc8
Rafael Oliveira Montagner Melatti <164571475+Romm-0@users.noreply.github.com>
2025-09-26 15:21:25 -0300

    [Feat] Adiciona searchbar para busca por palavras (#33)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import Title from "@/components/Title";
-//import SearchBar from "@/components/SearchBar";
+import SearchBar from "@/components/SearchBar";
 //import LoadCards from "@/components/LoadCards"; 
 import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
-        <Title />
-        {/* <SearchBar />
-        <LoadCards /> */}
-        <Footer /> {/* Ainda sem search e cards, no futuro ele ira lá para baixo */}
+      <Title />
+      <SearchBar />
+      {/* <LoadCards /> */}
+      <Footer />
     </>
   );
 }
