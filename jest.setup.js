5b74bc8542f275741b3c5c63094b34ffa4e9881b
Bryan70897 <bryansmith.cavalcante@gmail.com>
2025-11-30 13:02:29 -0300

    [Fix] Update Jest.setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,8 @@
 import '@testing-library/jest-dom';
 
-// Jest setup file - add lightweight mocks for Next.js modules used in tests
-// Enable testing-library DOM matchers (toBeInTheDocument, toHaveClass, etc.)
+
 require('@testing-library/jest-dom')
-// Mock next/image to render a normal <img/> in the test environment
+
 jest.mock('next/image', () => {
 	const React = require('react')
 	return {
@@ -11,13 +10,12 @@ jest.mock('next/image', () => {
 		default: (props) => {
 			// Render a plain img and avoid passing boolean-only props like `fill`/`priority`
 			const { src, alt, className, sizes } = props || {}
-			// eslint-disable-next-line jsx-a11y/alt-text
+		
 			return React.createElement('img', { src, alt, className, sizes })
 		},
 	}
 })
 
-// Optionally mock next-themes ThemeProvider to simply render children in tests
 jest.mock('next-themes', () => {
 	const React = require('react')
 	return {
